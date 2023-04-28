c6068e7e5471020cc4046c33db86b47e24cc853e
Alexandre Devert <marmakoide@hotmail.fr>
2023-04-28 17:33:27 +0200

    Updated documentation generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath']
+extensions = ['sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ESKit'
-copyright = u'2010, Alexandre Devert'
+copyright = u'2009-2023, Alexandre Devert'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
