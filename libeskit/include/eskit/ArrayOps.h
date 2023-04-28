5f0864a814551296b7e1e7187d198b7b49c9607d
Alexandre Devert <marmakoide@hotmail.fr>
2023-04-28 16:01:57 +0200

    Updated .gitignore

--- libeskit/include/eskit/ArrayOps.h
+++ libeskit/include/eskit/ArrayOps.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Alexandre Devert <marmakoide@yahoo.fr>
+ * Copyright (c) 2010-2023 Alexandre Devert <marmakoide@hotmail.fr>
  *
  * ESKit is free software; you can redistribute it and/or modify it under the 
  * terms of the MIT license. See LICENSE for details.
